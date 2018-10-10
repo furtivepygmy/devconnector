02388fe485c7b3196eb9122252a3abfb19ab3f25
ASHENHEART\rzv <rzvgib001@gmail.com>
2018-10-10 11:11:28 +0400

    Second commit with registration endpoint

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../../models/User');
 
 // @route   GET api/auth/test
 // @desc    Tests Auth route
 // @access  Public
 router.get('/test', (req, res) => res.json({ message: 'Auth route works' }));
+
+// @route   GET api/auth/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  // first find if the email exists using findOne
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+
+      // create a new resource with mongoose
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
