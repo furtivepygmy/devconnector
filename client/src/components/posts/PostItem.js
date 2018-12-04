224f79132aeedd3cdce92b94fbfa1bcd9ffed22e
Furtive Pygmy <rzvgib001@gmail.com>
2018-12-04 05:14:53 +0400

    Redeploy

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { deletePost, addLike, removeLike } from '../../actions/postActions';
-import Post from '../post/Post';
 
 class PostItem extends Component {
   onDeleteClick = id => {
@@ -35,29 +34,29 @@ class PostItem extends Component {
     const { post, auth, showActions } = this.props;
 
     return (
-      <div className='card card-body mb-3'>
-        <div className='row'>
-          <div className='col-md-2'>
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
             <Link to={`/profile/${post.handle}`}>
               <img
-                className='rounded-circle d-none d-md-block'
+                className="rounded-circle d-none d-md-block"
                 src={post.avatar}
-                alt=''
+                alt=""
               />
             </Link>
             <br />
             <Link to={`/profile/${post.handle}`}>
-              <p className='text-center'>{post.name}</p>
+              <p className="text-center">{post.name}</p>
             </Link>
           </div>
-          <div className='col-md-10'>
-            <p className='lead'>{post.text}</p>
+          <div className="col-md-10">
+            <p className="lead">{post.text}</p>
             {showActions ? (
               <span>
                 <button
                   onClick={() => this.onLikeClick(post._id)}
-                  type='button'
-                  className='btn btn-light mr-1'
+                  type="button"
+                  className="btn btn-light mr-1"
                 >
                   <i
                     className={`${
@@ -66,25 +65,25 @@ class PostItem extends Component {
                         : 'text-secondary'
                     } fas fa-thumbs-up`}
                   />
-                  <span className='badge badge-light'>{post.likes.length}</span>
+                  <span className="badge badge-light">{post.likes.length}</span>
                 </button>
                 <button
                   onClick={() => this.onUnlikeClick(post._id)}
-                  type='button'
-                  className='btn btn-light mr-1'
+                  type="button"
+                  className="btn btn-light mr-1"
                 >
-                  <i className='text-secondary fas fa-thumbs-down' />
+                  <i className="text-secondary fas fa-thumbs-down" />
                 </button>
-                <Link to={`/post/${post._id}`} className='btn btn-info mr-1'>
+                <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
                   Comments
                 </Link>
                 {post.user === auth.user.id ? (
                   <button
                     onClick={() => this.onDeleteClick(post._id)}
-                    type='button'
-                    className='btn btn-danger mr-1'
+                    type="button"
+                    className="btn btn-danger mr-1"
                   >
-                    <i className='fas fa-times' />
+                    <i className="fas fa-times" />
                   </button>
                 ) : null}
               </span>
